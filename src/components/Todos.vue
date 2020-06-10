3ae6f838394aa35774f5a440b6f02f32575ddd61
Philippe Fonzin <pfonzin@gmail.com>
2020-06-10 12:28:19 -0400

    Create TodoItem

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -8,14 +8,20 @@ This is a serperate component to display the todos
     to each todo. -->
 
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <h3>{{todo.title}}</h3>
+      <!-- as we iterate we bind each todo to a TodoItem  -->
+      <TodoItem v-bind:todo="todo"/> 
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem"; // import TodoItem 
+
 export default {
   name: "Todos",
+  components: {
+    TodoItem // Add TodoItem to components 
+  },
   // Add the todos props that are being passed in from App.vue. Now we should have 
   // access to them in the template above.
   props: ["todos"]
